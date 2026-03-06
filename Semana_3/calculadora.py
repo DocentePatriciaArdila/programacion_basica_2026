59f7ab966dea624e1a4e18f1ec138a65846d4481
DocentePatriciaArdila <patricia.ardila.pa@uniminuto.edu.co>
2026-03-06 15:54:18 -0500

    calculadora.py

---
+++ Semana_3/calculadora.py
@@ -0,0 +1,18 @@
+#este es un comentario
+#primer programa
+a=7
+b=8
+suma=a+b
+print('la suma de',a,'y',b,'es',suma)
+print('la resta de',a,'y',b,'es',a-b)
+print('la multiplicacion de',a,'y',b,'es',a*b)
+print('la division de',a,'y',b,'es',a/b)
+print('la potencia de',a,'y',b,'es',a**b)
+print('el modulo de',a,'y',b,'es',a%b)
+print('la division entera de',a,'y',b,'es',a//b)
+
+print(a>b)
+print("a es mayor que b?",a>b)
+print("a es menor que b?",a<b)
+print("a es igual a b?",a==b)
+print("a es diferente de b?",a!=b)
