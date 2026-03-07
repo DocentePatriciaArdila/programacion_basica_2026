0ae3697a4d65fe9df58d6808b8577553fb11ad75
DocentePatriciaArdila <patricia.ardila.pa@uniminuto.edu.co>
2026-03-06 20:57:04 -0500

    primer programa

---
+++ semana3/ejer1.py
@@ -0,0 +1,16 @@
+#esto es un tiponde dato float
+a=3
+#esto es un tipo de dato entero
+b=300
+print("la suma es:",a+b)
+print("a resta es:",a-b)
+print("la multiplicacion es:",a*b)
+print("la division es:",a/b)
+print("la division entera es:",a//b)
+#residuo de la division
+print("el residuo es:",a%b)
+
+#comparaciones
+print(a<b)
+print(a>b)
+print(a==b)
